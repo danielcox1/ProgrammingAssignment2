a26e8ac6cbb79cbaae85e6761c91a3c9000a7fa8
Daniel Bizer-Cox <Bizer-Cox.Daniel@epa.gov>
2016-10-28 13:10:43 -0400

    completed cache matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the user to cache the inverse of a matrix.
+## This can save computation time if the inverse is needed many times.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a matrix object that can cache its inverse
+makeCacheMatrix <- function(x=matrix()) {
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Compute the inverse of the matrix or return the cached calculation if available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        #message("getting inverse from cache...")
+        return(i)
+    }
+    m <- x$get()
+    i <- solve(m, ...)
+    x$setInverse(i)
+    return(i)
 }
+
+# Test code to prove cache is faster than recomputing
+# options(max.print=1)
+# m <- matrix(data=rnorm(10*10), nrow=10, ncol=10)
+# cm <- makeCacheMatrix(m)
+
+# ptm <- proc.time()
+# replicate(10000, {solve(m)})
+# proc.time() - ptm
+
+# ptm <- proc.time()
+# replicate(10000, {cacheSolve(cm)})
+# proc.time() - ptm
